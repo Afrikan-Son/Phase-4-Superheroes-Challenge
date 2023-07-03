525ce26f743781df743f7b215b3a9a4f8554484a
Mark Wanjau <theafrikanson@gmail.com>
2023-07-03 22:22:48 +0300

    set heros route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :hero_powers
   resources :powers
-  resources :heros
+  
+  get "heroes", to: "heros#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
