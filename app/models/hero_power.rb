8ede6c3fd95236c61d03017d42369e4c6600616c
Mark Wanjau <theafrikanson@gmail.com>
2023-07-03 20:14:49 +0300

    validate strength

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,2 +1,8 @@
 class HeroPower < ApplicationRecord
+    validates :strength, inclusion:{
+        in: ['Strong', 'Weak', 'Average'],
+        message: "%{value} is not in the provided list"
+    }
+    belongs_to :power
+    belongs_to :hero
 end
