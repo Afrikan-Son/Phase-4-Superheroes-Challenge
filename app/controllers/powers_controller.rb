c1032ced33e936d0ae3acd79d4bb2c1c4ffcff57
Mark Wanjau <theafrikanson@gmail.com>
2023-07-04 00:02:49 +0300

    Get powers id and show in the route

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,6 +1,19 @@
 class PowersController < ApplicationController
 
+
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
     def index
         render json: Power.all, status: :ok
+    end
 
+    def show
+        power=Power.find_by!(id:params[:id])
+        render json: power, status: :ok
+    end
+
+
+    private
+    def not_found_response
+        render json: {error: "Power not found"}, status: :not_found
+    end
 end
