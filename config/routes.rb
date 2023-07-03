1c489bad9eeae0185a656b59712dad50886794e2
Mark Wanjau <theafrikanson@gmail.com>
2023-07-03 22:30:45 +0300

    add route to show a hero by ID

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :powers
   
   get "heroes", to: "heros#index"
+  get "heroes/:id", to: "heros#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
